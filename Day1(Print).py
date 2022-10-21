348666e1a5cf079af9f0960bbba20b79bc2e0ce0
Shreyash Somvanshi <91454227+ShreyashSomvanshi@users.noreply.github.com>
2022-10-22 00:05:20 +0530

    Create Day1(Print).py

---
+++ Day1(Print).py
@@ -0,0 +1,13 @@
+print("Hello, World!")
+print("Shreyash Somvanshi")
+print("21st October 2022")
+
+print("""
+I am signing up for Replit's 100 Days of Python
+challenge!
+I will make sure to spend some time everyday coding along, for a minimum of 10 minutes a day.
+I'll be using Replit, an amazing online IDE so I can do this from my phone whenever I happen to be. No excuses for not coding from the middle of field!
+I am feeling 🤟💪
+You can follow my progress at replit.com/@shreyash1363
+""")
+
