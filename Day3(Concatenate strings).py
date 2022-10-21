d5f9235f6473d0244aa8233c4f1347c466de8563
Shreyash Somvanshi <91454227+ShreyashSomvanshi@users.noreply.github.com>
2022-10-22 00:42:07 +0530

    Create Day3(Concatenate strings).py

---
+++ Day3(Concatenate strings).py
@@ -0,0 +1,8 @@
+food=input("Name a food > ")
+plant = input("Name a type of plant > ")
+met = input("Name a method of cooking > ")
+word = input("What word describes your ruined food? > ")
+item = input("Name a DIY item > ")
+
+print("MENU")
+print("Saute", food, "with", word, plant, "on a bed of", item)
