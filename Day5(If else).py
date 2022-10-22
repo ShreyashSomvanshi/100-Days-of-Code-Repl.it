5d94f4d034207afd32ce7e5fabae24182e6a83aa
Shreyash Somvanshi <91454227+ShreyashSomvanshi@users.noreply.github.com>
2022-10-22 14:52:00 +0530

    Create Day5(If else).py

---
+++ Day5(If else).py
@@ -0,0 +1,20 @@
+print("Marvel Movie Character Creator")
+print("__")
+print()
+i = input("Do you like 'hanging around'?: ")
+if i == "Yes":
+  print("You're Spiderman!")
+else:
+  print("You're not Spiderman!")
+
+j = input("Do you have 'gravely'voice?: ")
+if j == "Yes":
+  print("You're Korg!")
+else:
+  print("You're not korg!")
+
+k = input("Do you often feel 'marvelous'?: ")
+if k == "Yes":
+  print("You're Captain Marvel!")
+else:
+  print("You're not Captain Marvel!")
