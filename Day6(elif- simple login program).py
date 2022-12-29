706ba7705a916bd6e9b9ee9171d9e33c9b07a2f7
Shreyash Somvanshi <91454227+ShreyashSomvanshi@users.noreply.github.com>
2022-12-29 23:05:38 +0530

    Create Day6(elif- simple login program).py

---
+++ Day6(elif- simple login program).py
@@ -0,0 +1,15 @@
+print("SECURE LOGIN")
+username = input("Username > ")
+password = input("Password> ")
+
+if username == "mark" and password == "password":
+ print("Welcome Mark!")
+  
+elif username == "suzanne"and password == 'Su74nne':
+ print("Hey there Suzanne!")
+
+elif username == "shreyash"and password == 'Shreyashhh':
+ print("Hey there Shreyash!")
+  
+else:
+ print("Go away!")
