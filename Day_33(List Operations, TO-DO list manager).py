af07b1dd1e252441de185a757ce87defdf26f2c3
Shreyash Somvanshi <91454227+ShreyashSomvanshi@users.noreply.github.com>
2023-01-02 11:17:37 +0530

    Create Day_33(List Operations, TO-DO list manager).py

---
+++ Day_33(List Operations, TO-DO list manager).py
@@ -0,0 +1,65 @@
+###################################################################################
+# Dynamic lists are ways of using a blank list and adding or removing items to it as we go.
+# Append
+
+myAgenda = []
+
+def printList():
+  print() #this is just to add an extra space between items
+  for item in myAgenda:
+    print(item)
+  print() #this is just to add an extra space between items
+
+while True:
+  item = input("What's next on the Agenda?: ")
+  myAgenda.append(item)
+  printList()
+
+
+###################################################################################
+# Remove items from a list
+myAgenda = []
+
+def printList():
+  print() 
+  for item in myAgenda:
+    print(item)
+  print() 
+
+while True:
+  menu = input("add or remove?: ")
+  if menu == "add":
+    item = input("What's next on the Agenda?: ")
+    myAgenda.append(item)
+  elif menu == "remove":
+    item = input("What do you want to remove?: ")
+    myAgenda.remove(item)
+  printList()
+
+
+
+###################################################################################
+# TO DO LIST MANAGER
+import os, time
+toDoList = []
+
+def printList():
+  print()
+  for item in toDoList:
+    print(item)
+  print()
+
+while True:
+  menu = input("ToDoList Manager\n\nDo you want to view, add or edit the todo list?\n")
+  if menu=="view":
+    printList()
+  elif menu=="add":
+    item = input("What do you want to add?\n")
+    toDoList.append(item)
+  elif menu=="edit":
+    item = input("What do you want to remove?\n")
+    if item in toDoList:
+      toDoList.remove(item)
+
+
+###################################################################################
